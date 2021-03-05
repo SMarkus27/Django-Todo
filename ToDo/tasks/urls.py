6a76d245d3582f70f4410a17ffd2e5d7379554c0
SMarkus27 <pereira.markus.27@hotmail.com>
2021-03-05 16:11:40 -0300

    create the functions to delete and edit

--- ToDo/tasks/urls.py
+++ ToDo/tasks/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.show_tasks),
-    path("submit/", views.create_tasks),
+    path("submit/", views.create_task),
+    path('edit/',views.edit_task),
+    path('edit/submit/',views.submit_edit),
+    path('delete/<id>', views.delete_task),
 ]
--- ToDo/tasks/views.py
+++ ToDo/tasks/views.py
@@ -6,11 +6,38 @@ def show_tasks(request):
     data = {'tasks':tasks}
     return render(request,'index.html',data)
 
-def create_tasks(request):
+
+#create the tasks
+def create_task(request):
     task = request.POST.get('task')
     if request.POST:
         if task == "":
             return redirect('/')
         else:
             Tasks.objects.create(task=task)
+    return redirect('/')
+    
+#show the task for edit
+def edit_task(request):
+    id_task = request.GET.get('id')
+    dados = {}
+    if id_task:
+        dados['edit']= Tasks.objects.get(id=id_task)
+        return render(request, 'index.html',dados)
+
+#edit the task
+def submit_edit(request):
+    task = request.POST.get('task')
+    id_task = request.POST.get('id_task')
+    if id_task:
+        if task == "":
+            return redirect('/')
+        else:
+            Tasks.objects.filter(id=id_task).update(task=task)
+            return redirect('/')
+
+
+#delete tasks
+def delete_task(request,id):
+    Tasks.objects.get(id=id).delete()
     return redirect('/')
